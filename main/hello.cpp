c7e3151a9f0e3ff67de8d33c94f13003e95ea467
anaghasid <106751197+anaghasid@users.noreply.github.com>
2024-03-08 19:55:32 +0530

    changes

--- main/hello.cpp
+++ main/hello.cpp
@@ -5,6 +5,7 @@ using namespace std;
 int main(void) {
   cout << "Hello, World, this is PES1UG21CS075\n";
   cout << "Hello, Jenkins\n";
-  cout << "I have successfully built and run\n";
+  // cout << "I have successfully built and run\n";
+  cout<<"This is my second commit\n";
   return 0;
 }
